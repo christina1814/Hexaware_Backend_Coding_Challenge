8a04e76079d6f8c8404ea3d18e80df4903435340
Christina <christinamr1814@gmail.com>
2025-06-11 12:06:40 +0530

    backend-java

--- src/main/java/com/hexaware/cricketmanagement/CricketTeamManagementSystemApplication.java
+++ src/main/java/com/hexaware/cricketmanagement/CricketTeamManagementSystemApplication.java
@@ -1,3 +1,10 @@
+/**
+ * ============================================================================
+ * Author      : Christina Michelle Rayan D
+ * Created On  : 2025-06-02
+ * Description : This project performs View, Add, Update, Delete Players operations.
+ * ============================================================================
+ */
 package com.hexaware.cricketmanagement;
 
 import org.springframework.boot.SpringApplication;
