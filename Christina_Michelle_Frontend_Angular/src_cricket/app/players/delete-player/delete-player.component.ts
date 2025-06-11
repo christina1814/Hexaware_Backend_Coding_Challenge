eb727bdad2ea449f36c4a0d4a6299e97b1be03e0
Christina <christinamr1814@gmail.com>
2025-06-11 12:18:48 +0530

    Frontend-Angular

---
+++ Christina_Michelle_Frontend_Angular/src_cricket/app/players/delete-player/delete-player.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { PlayerService } from 'src/app/services/player.service';
+
+@Component({
+  selector: 'app-player-delete',
+  templateUrl: './delete-player.component.html'
+})
+export class DeletePlayerComponent {
+  playerId: number = 0;
+  message: string = '';
+
+  constructor(private service: PlayerService) {}
+
+  deletePlayer() {
+    if (!this.playerId) {
+      alert('Please enter a valid Player ID.');
+      return;
+    }
+
+   this.service.deletePlayer(this.playerId)
+   .subscribe(
+    () => {alert(`Player with ID ${this.playerId} deleted successfully!`);},
+
+    () => {
+      alert(`Player with ID ${this.playerId} not found`);
+    });
+  }
+}
